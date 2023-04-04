5ed8932da71d5a3c1dcf9a465c28367ce53ad977
possiblyMikeB <miblackmon@davidson.edu>
2023-04-04 14:32:48 -0400

    upate extension name and prefs options.

--- src/prefs.js
+++ src/prefs.js
@@ -2,14 +2,14 @@
 // Only does key namespacing with a prefix, and defaults when pref is not set
 function getPref(name, def) {
     // Namespace our preference keys
-    const key = `nbgitpuller-link-generator-${name}`;
+    const key = `ncshare-link-generator-${name}`;
     const value = localStorage.getItem(key);
     return value === null ? def : value;
 }
 
 function setPref(name, value) {
     // Namespace our preference keys
-    const key = `nbgitpuller-link-generator-${name}`;
+    const key = `ncshare-link-generator-${name}`;
     localStorage.setItem(key, value);
 }
 
